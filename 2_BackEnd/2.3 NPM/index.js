14e3742dbcdd7df5253ce579b6f7acb544390796
Mrityunjoy <134674188+Pikxul@users.noreply.github.com>
2024-06-04 21:12:58 +0530

    Delete 2_BackEnd directory

--- 2_BackEnd/2.3 NPM/index.js
+++
@@ -1,4 +0,0 @@
-import superheroes from "superheroes"    // first superheros is object | second superhero is package
-var superheroname = superheroes.random();
-
-console.log(`im ${superheroname}. `);
